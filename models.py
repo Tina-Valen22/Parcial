f0adc5386100fdde5b48bbce5c0ee4dd1496c926
Valentina Ovalle Osorio <vovalle72@ucatolica.edu.co>
2025-10-29 21:22:18 -0500

    EmpleadoBase, estructura básica y validaciones de cada campo.

--- models.py
+++ models.py
@@ -9,3 +9,9 @@ proyecto_empleado = Table(
     Column("proyecto_id", ForeignKey("proyecto.id"), primary_key=True),
     Column("empleado_id", ForeignKey("empleado.id"), primary_key=True),
 )
+
+class EmpleadoBase(SQLModel):
+    nombre: constr(min_length=2, max_length=100)
+    especialidad: constr(min_length=2, max_length=80)
+    salario: condecimal(gt=0)
+    estado: constr(min_length=3, max_length=20)
