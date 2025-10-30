899a2a3476dd7efbfc5e5411bf325a8e2ff0e7fe
Valentina Ovalle Osorio <vovalle72@ucatolica.edu.co>
2025-10-29 19:20:40 -0500

    Se crean las clases de batos para conectarse a sqlite

---
+++ database.py
@@ -0,0 +1,16 @@
+import os
+from sqlmodel import SQLModel, create_engine, Session
+from typing import Generator
+from dotenv import load_dotenv
+
+load_dotenv()
+
+DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///./proyectos.db")
+engine = create_engine(DATABASE_URL, echo=False, connect_args={"check_same_thread": False} if "sqlite" in DATABASE_URL else {})
+
+def init_db() -> None:
+    SQLModel.metadata.create_all(engine)
+
+def get_session() -> Generator[Session, None, None]:
+    with Session(engine) as session:
+        yield session
