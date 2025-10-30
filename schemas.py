e73c1a2afedf3340bbcaaaae5a110a58ccfa4f74
Valentina Ovalle Osorio <vovalle72@ucatolica.edu.co>
2025-10-29 20:55:19 -0500

    Se crea la clase EmpleadoUpdate

--- schemas.py
+++ schemas.py
@@ -11,3 +11,13 @@ class EmpleadoCreate(SQLModel):
     @validator("estado")
     def estado_ok(cls, v):
         return v.lower()
+    
+class EmpleadoUpdate(SQLModel):
+    nombre: Optional[constr(min_length=2, max_length=100)]
+    especialidad: Optional[constr(min_length=2, max_length=80)]
+    salario: Optional[condecimal(gt=0)]
+    estado: Optional[constr(min_length=3, max_length=20)]
+
+    @validator("estado")
+    def estado_ok(cls, v):
+        return v.lower()
