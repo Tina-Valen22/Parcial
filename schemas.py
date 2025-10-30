f9f8e41e30fdc15b2d3e91d148085fb1a5e4c07a
Valentina Ovalle Osorio <vovalle72@ucatolica.edu.co>
2025-10-29 19:25:25 -0500

    Se crean clases para crear empleador

---
+++ schemas.py
@@ -0,0 +1,13 @@
+from typing import List, Optional
+from sqlmodel import SQLModel, Field
+from pydantic import validator, condecimal, constr
+
+class EmpleadoCreate(SQLModel):
+    nombre: constr(min_length=2, max_length=100)
+    especialidad: constr(min_length=2, max_length=80)
+    salario: condecimal(gt=0)
+    estado: constr(min_length=3, max_length=20)
+
+    @validator("estado")
+    def estado_ok(cls, v):
+        return v.lower()
